79fbaf3a5530fa5e75353ac53fabc2c410c9b722
pingwin <jaloszewski.adrian@gmail.com>
2016-12-17 11:24:32 +0100

    Project creation

---
+++ src/main/java/com/github/tastypenguinbacon/hackaton/main/Main.java
@@ -0,0 +1,10 @@
+package com.github.tastypenguinbacon.hackaton.main;
+
+/**
+ * Created by pingwin on 17.12.16.
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("cudo");
+    }
+}
