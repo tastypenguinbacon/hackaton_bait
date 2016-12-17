29072b5863610134705a9e213f8d543991676d54
pingwin <jaloszewski.adrian@gmail.com>
2016-12-17 12:07:49 +0100

    Added spring support

--- src/main/java/com/github/tastypenguinbacon/hackaton/main/Main.java
+++ src/main/java/com/github/tastypenguinbacon/hackaton/main/Main.java
@@ -1,10 +1,14 @@
 package com.github.tastypenguinbacon.hackaton.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by pingwin on 17.12.16.
  */
+@SpringBootApplication
 public class Main {
     public static void main(String... args) {
-        System.out.println("cudo");
+        SpringApplication.run(Main.class, args);
     }
 }
