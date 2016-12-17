31ffb7d46cf2279eb72b0be7d63df725359c4c14
pingwin <jaloszewski.adrian@gmail.com>
2016-12-17 20:18:41 +0100

    Changed api mappings

--- src/main/java/com/github/tastypenguinbacon/hackaton/controller/CheatSheetController.java
+++ src/main/java/com/github/tastypenguinbacon/hackaton/controller/CheatSheetController.java
@@ -1,31 +1,32 @@
 package com.github.tastypenguinbacon.hackaton.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tastypenguinbacon.hackaton.data.CheatSheet;
-import com.github.tastypenguinbacon.hackaton.database.DataBaseConnection;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by pingwin on 17.12.16.
  */
 @RestController
-@RequestMapping("cheatsheet")
 public class CheatSheetController {
 
-    @RequestMapping(value = "/query")
-    public String getCheatSheetAccordingToRegex(@RequestParam String regex) throws JsonProcessingException {
-        DataBaseConnection db = new DataBaseConnection();
-        CheatSheet cheatSheet = db.getCheatSheetByRegex(regex);
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(cheatSheet);
+    @RequestMapping(value = "/cheat_sheet", method = RequestMethod.GET)
+    public List<String> getCheatSheetNames() {
+        return null;
+    }
+
+    @RequestMapping(value = "/{user}/cheat_sheet/{cheatsheetName}",
+            method = RequestMethod.GET)
+    public String getCheatSheetAccordingToRegex(@RequestParam String like)
+            throws JsonProcessingException {
+        return null;
     }
 
     @RequestMapping(value = "/{cheatSheetName}", method = RequestMethod.POST)
     public String addElementsToCheatSheet(@PathVariable String cheatSheetName,
                                           @RequestBody CheatSheet body) {
-        DataBaseConnection db = new DataBaseConnection();
-        boolean successful = db.addCheatSheet(cheatSheetName, body);
-        return successful ? "SUCCESS" : "FAIL";
+        return null;
     }
 }
